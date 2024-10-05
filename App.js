c8c6019d45ee02bfd20aa2755d88b967df12b19f
Leah Deshar <leah.desar@gmail.com>
2024-10-05 22:47:30 +0545

    story header

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ import {
   SafeAreaView,
   TextInput,
   Pressable,
+  KeyboardAvoidingView,
+  Platform,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import userStories from "./stories";
@@ -19,6 +21,7 @@ import Animated, {
   useAnimatedReaction,
   runOnJS,
 } from "react-native-reanimated";
+import { AntDesign, Feather, MaterialCommunityIcons } from "@expo/vector-icons";
 
 const uri =
   "https://images.unsplash.com/photo-1472586662442-3eec04b9dbda?q=80&w=1774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
@@ -71,13 +74,6 @@ const App = () => {
     });
   };
 
-  // useEffect(() => {
-  //   progress.value = 0;
-  //   progress.value = withTiming(1, {
-  //     duration: storyViewDuration,
-  //     easing: Easing.linear,
-  //   });
-  // }, [storyIndex]);
   useEffect(() => {
     progress.value = 0;
     progress.value = withTiming(1, {
@@ -99,119 +95,220 @@ const App = () => {
   }));
 
   return (
-    <SafeAreaView
+    <KeyboardAvoidingView
       style={{
         flex: 1,
-        backgroundColor: "black",
       }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      enabled
     >
-      <View style={{ flex: 1 }}>
-        <Image
-          source={{ uri: story?.uri }}
-          style={{
-            width: "100%",
-            height: "100%",
-            borderRadius: 10,
-          }}
-        />
-        <Pressable
-          style={{
-            position: "absolute",
-            // backgroundColor: "red",
-            width: "30%",
-            height: "100%",
-          }}
-          onPress={goToPrevStory}
-        />
-        <Pressable
-          style={{
-            position: "absolute",
-            right: 0,
-            // backgroundColor: "red",
-            width: "30%",
-            height: "100%",
-          }}
-          onPress={goToNextStory}
-        />
-        <View
-          style={{
-            position: "absolute",
-            top: 0,
-            width: "100%",
-            padding: 20,
-            paddingTop: 5,
-          }}
-        >
-          <LinearGradient
-            colors={["rgba(0,0,0,0.7)", "transparent"]}
-            style={StyleSheet.absoluteFill}
-          />
-
-          <View
-            style={{
-              gap: 5,
-              flexDirection: "row",
-              marginBottom: 20,
-            }}
-          >
-            {user.stories.map((story, index) => (
-              <View
-                key={index}
-                style={{
-                  flex: 1,
-                  height: 2,
-                  backgroundColor: "gray",
-                  borderRadius: 10,
-                  overflow: "hidden",
-                }}
-              >
-                <Animated.View
-                  style={[
-                    {
-                      backgroundColor: "white",
-                      height: "100%",
-                    },
-                    index === storyIndex && indicatorAnimatedStyle,
-                    index > storyIndex && { width: 0 },
-                    index < storyIndex && { width: "100%" },
-                  ]}
-                />
-              </View>
-            ))}
-          </View>
-          <Text
-            style={{
-              color: "white",
-              fontWeight: "bold",
-              marginTop: 10,
-            }}
-          >
-            {user.username}
-          </Text>
-        </View>
-      </View>
-      <View
+      <SafeAreaView
         style={{
-          height: 100,
-          width: "100%",
+          flex: 1,
           backgroundColor: "black",
-          padding: 10,
         }}
       >
-        <TextInput
+        <View style={{ flex: 1 }}>
+          <Image
+            source={{ uri: story?.uri }}
+            style={{
+              width: "100%",
+              height: "100%",
+              borderRadius: 10,
+            }}
+          />
+          <Pressable
+            style={{
+              position: "absolute",
+              // backgroundColor: "red",
+              width: "30%",
+              height: "100%",
+            }}
+            onPress={goToPrevStory}
+          />
+          <Pressable
+            style={{
+              position: "absolute",
+              right: 0,
+              // backgroundColor: "red",
+              width: "30%",
+              height: "100%",
+            }}
+            onPress={goToNextStory}
+          />
+          <View
+            style={{
+              position: "absolute",
+              top: 0,
+              width: "100%",
+              padding: 20,
+              paddingTop: 5,
+            }}
+          >
+            <LinearGradient
+              colors={["rgba(0,0,0,0.7)", "transparent"]}
+              style={StyleSheet.absoluteFill}
+            />
+
+            <View
+              style={{
+                gap: 5,
+                flexDirection: "row",
+                marginBottom: 10,
+                marginTop: 5,
+              }}
+            >
+              {user.stories.map((story, index) => (
+                <View
+                  key={index}
+                  style={{
+                    flex: 1,
+                    height: 2,
+                    backgroundColor: "gray",
+                    borderRadius: 10,
+                    overflow: "hidden",
+                  }}
+                >
+                  <Animated.View
+                    style={[
+                      {
+                        backgroundColor: "white",
+                        height: "100%",
+                      },
+                      index === storyIndex && indicatorAnimatedStyle,
+                      index > storyIndex && { width: 0 },
+                      index < storyIndex && { width: "100%" },
+                    ]}
+                  />
+                </View>
+              ))}
+            </View>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  gap: 10,
+                }}
+              >
+                <Image
+                  source={{ uri: user.userImg }}
+                  style={{
+                    width: 35,
+                    height: 35,
+                    borderRadius: 25,
+                  }}
+                />
+                <View>
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      alignItems: "center",
+                      gap: 10,
+                    }}
+                  >
+                    <Text
+                      style={{
+                        color: "white",
+                        fontWeight: "bold",
+                        // marginTop: 10,
+                      }}
+                    >
+                      {user.username}
+                    </Text>
+                    <Text
+                      style={{
+                        fontSize: 11,
+                        color: "white",
+                        marginTop: 2,
+                      }}
+                    >
+                      {story?.time}
+                    </Text>
+                  </View>
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      alignItems: "center",
+                      gap: 2,
+                      marginTop: 1.5,
+                    }}
+                  >
+                    <MaterialCommunityIcons
+                      name="movie-play"
+                      size={14}
+                      color="white"
+                    />
+                    <Text style={{ color: "white", fontSize: 13 }}>
+                      Watch full reel
+                    </Text>
+                    <AntDesign name="right" size={12} color="white" />
+                  </View>
+                </View>
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  gap: 20,
+                }}
+              >
+                <MaterialCommunityIcons
+                  name="dots-horizontal"
+                  size={20}
+                  color="white"
+                />
+                <AntDesign name="close" size={24} color="white" />
+              </View>
+            </View>
+          </View>
+        </View>
+        <View
           style={{
-            borderWidth: 1,
-            borderColor: "gray",
-            padding: 10,
-            borderRadius: 50,
-            color: "white",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+            // gap: 3,
+            height: 110,
           }}
-          placeholder="Send message"
-          placeholderTextColor="gray"
-        />
-      </View>
-      <StatusBar style="light" />
-    </SafeAreaView>
+        >
+          <View
+            style={{
+              width: "81%",
+              backgroundColor: "black",
+              marginRight: 9,
+            }}
+          >
+            <TextInput
+              style={{
+                borderWidth: 1,
+                borderColor: "gray",
+                padding: 10,
+                borderRadius: 50,
+                color: "white",
+              }}
+              placeholder="Send message..."
+              placeholderTextColor="gray"
+            />
+          </View>
+          <View
+            style={{
+              marginRight: 10,
+            }}
+          >
+            <AntDesign name="hearto" size={22} color="white" />
+          </View>
+          <Feather name="send" size={22} color="white" />
+        </View>
+        <StatusBar style="light" />
+      </SafeAreaView>
+    </KeyboardAvoidingView>
   );
 };
 
--- stories.js
+++ stories.js
@@ -2,6 +2,8 @@ export default userStories = [
   {
     userId: 1,
     username: "john_doe",
+    userImg:
+      "https://images.unsplash.com/photo-1521310192545-4ac7951413f0?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     stories: [
       {
         uri: "https://images.unsplash.com/photo-1593034509785-5b17ba49f683?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -14,27 +16,37 @@ export default userStories = [
   {
     userId: 2,
     username: "jane_smith",
+    userImg:
+      "https://images.unsplash.com/photo-1523111567642-f71bebeb173f?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     stories: [
       {
         uri: "https://images.unsplash.com/photo-1521232002294-6d1cb35b6f8c?q=80&w=1810&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        time: "7h",
       },
       {
         uri: "https://images.unsplash.com/photo-1607517024458-011434a12cbd?q=80&w=1927&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        time: "3h",
       },
       {
         uri: "https://images.unsplash.com/photo-1521232002294-6d1cb35b6f8c?q=80&w=1810&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        time: "8h",
       },
       {
         uri: "https://images.unsplash.com/photo-1607517024458-011434a12cbd?q=80&w=1927&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        time: "8h",
       },
     ],
   },
   {
     userId: 3,
     username: "mike_the_great",
+    userImg:
+      "https://images.unsplash.com/photo-1527418124353-ca783e9d1d1a?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+
     stories: [
       {
         uri: "https://images.unsplash.com/photo-1518469164615-97026679ea59?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        time: "2h",
       },
     ],
   },
